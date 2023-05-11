8d072346c58c9fbf5e7af5b497deb37de558fdf6
mmaciasdisbyte <mmacias@disbyte.com>
2023-05-11 12:27:47 -0300

    add services and front

--- FirstBlazorApp/Client/Program.cs
+++ FirstBlazorApp/Client/Program.cs
@@ -1,10 +1,13 @@
 using FirstBlazorApp.Client;
+using FirstBlazorApp.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IContactsService, ContactsService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ FirstBlazorApp/Client/Services/ContactsService.cs
@@ -0,0 +1,35 @@
+﻿using FirstBlazorApp.Shared;
+using System.Net.Http.Json;
+
+namespace FirstBlazorApp.Client.Services
+{
+    public class ContactsService : IContactsService
+    {
+        private readonly HttpClient _httpClient; //metodo desde que nos permite http request desde el codigo
+
+        public ContactsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Contact>> GetAll()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Contact>>($"api/contacts");
+        }
+
+        public async Task<Contact> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Save(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
